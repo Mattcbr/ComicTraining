4543c6e9fc45a4d544945dc5362c340d5a567116
Matheus Castelo <matheus.queiroz@ingressorapido.com.br>
2018-01-12 11:09:22 -0200

    Starting the project

---
+++ Comic Training/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Comic Training
+//
+//  Created by Matheus Castelo on 12/01/2018.
+//  Copyright © 2018 Matheus Castelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
